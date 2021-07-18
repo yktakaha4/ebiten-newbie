b2dac9f45d50ce12397c69cd7787cde4fb8928ff
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-07-18 18:48:46 +0900

    draw by ticks

--- hello/main.go
+++ hello/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	tick int64
+}
 
 func (g *Game) Update() error {
+	g.tick += 1
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	if (g.tick/10)%2 == 0 {
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("Hello, World! tick=%v", g.tick))
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
